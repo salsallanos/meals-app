ccf4a7c6af33ab22b6f73e1003709da30d5da1a8
Juan C Llanos <jllanos@betternoi.com>
2022-04-18 17:49:34 -0400

    progress on meal app

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,6 @@
 import React from "react";
-import classes from './MealItem.module.css';
+import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
   return (
@@ -10,9 +11,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-          <form>
-
-          </form>
+        <MealItemForm id={props.id}/>
       </div>
     </li>
   );
